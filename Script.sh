7c7ccf703d557b92a503d57a56f55568acb02443
thalescherubino <33007800+thalescherubino@users.noreply.github.com>
2021-05-03 20:40:01 -0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -32,6 +32,11 @@ rm `basename -s .gz $library` `basename -s 1.fq.gz $library`2.fq
 #samtools view -F 4 -@ 8 `basename -s 1.fq.gz $library`Aligned.out.bam > `basename -s 1.fq.gz $library`onlyMapped.bam
 #not a fuking good idea the last command, for some reason the resulting file was file 3 times bigger than the original 
 
+#Mark dup with picard
 
+#The program can take either coordinate-sorted or query-sorted inputs, however the behavior is slightly different. When the input is coordinate-sorted, unmapped mates of mapped records and supplementary/secondary alignments are not marked as duplicates. However, when the input is query-sorted (actually query-grouped), then unmapped mates and secondary/supplementary reads are not excluded from the duplication test and can be marked as duplicate reads.
+
+
+java -XX:ParallelGCThreads=8 -jar ~/bin/picard.jar SortSam I=`basename -s 1.fq.gz $library`Aligned.out.bam O=`basename -s 1.fq.gz $library`sorted.bam SORT_ORDER=queryname
 
 done
