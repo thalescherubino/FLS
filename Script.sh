ac201ade0f232649cbf15ea13b2512538851a7bd
thalescherubino <33007800+thalescherubino@users.noreply.github.com>
2021-05-03 16:30:07 -0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -8,11 +8,13 @@ cd
 
 /home/carabica/bin/STAR/source/STAR runThreadN 24 --runMode genomeGenerate --genomeDir ./ --sjdbGTFfile ./ GCF_003713225.1_Cara_1.0_genomic.gff --sjdbGTFtagExonParentTranscript Parent --sjdbGTFfeatureExon exon --genomeFastaFiles ./GCF_003713225.1_Cara_1.0_genomic.fna --genomeSAindexNbases 13
 
-for library in 'ls /media/newhd/teste/*1_fq.gz'
+allPath=/media/newhd/teste/
+
+for library in `ls /media/newhd/teste/*1_fq.gz`
 do
   echo $library
-pigz -p 8 -d -f -k $library > `basename -s .gz $library`
+#pigz -p 8 -d -f -k $allPath$library > `basename -s .gz $library`
 
-pigz -p 8 -d -f -k `basename -s 1_fq.gz $library`2_fq.gz > `basename -s 1_fq.gz $library`2_fq `
+#pigz -p 8 -d -f -k `basename -s 1_fq.gz $library`2_fq.gz > `basename -s 1_fq.gz $library`2_fq `
 
 done
